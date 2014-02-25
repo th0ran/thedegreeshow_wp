687c0e719a637f6d3632ff0944ea2433814936ca
th0ran <torarne.njaamo@gmail.com>
2014-02-25 14:32:20 +0000

    Adding styles for the single page

--- single.php
+++ single.php
@@ -2,7 +2,7 @@
 
 			<div id="content">
 
-				<div id="inner-content" class="wrap clearfix">
+				<div id="inner-content" class="wrap center-block clearfix">
 
 					<div id="main" class="eightcol first clearfix" role="main">
 						
@@ -16,22 +16,22 @@
 	</div>
 
 
-							<article class="center-block" id="post-<?php the_ID(); ?>" <?php post_class('clearfix'); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
+							<article id="post-<?php the_ID(); ?>" <?php post_class('clearfix'); ?> role="article" class="center-block" itemscope itemtype="http://schema.org/BlogPosting">
 
 								<header class="article-header">
 									
-									<h1 class="entry-title single-title" itemprop="headline"><?php the_title(); ?></h1>
-									<p class="byline vcard"><?php
+									<h1 class="center-block entry-title single-title" itemprop="headline"><?php the_title(); ?></h1>
+									<p class="center-block byline vcard"><?php
 										printf( __( 'Posted <time class="updated" datetime="%1$s" pubdate>%2$s</time> by <span class="author">%3$s</span> <span class="amp">&amp;</span> filed under %4$s.', 'bonestheme' ), get_the_time( 'Y-m-j' ), get_the_time( get_option('date_format')), bones_get_the_author_posts_link(), get_the_category_list(', ') );
 									?></p>
 
 								</header>
 
-								<section class="entry-content clearfix" itemprop="articleBody">									
+								<section class="center-block entry-content clearfix" itemprop="articleBody">									
 									<?php the_content(); ?>
 								</section>
 
-								<footer class="article-footer">
+								<footer class="center-block article-footer">
 									<?php the_tags( '<p class="tags"><span class="tags-title">' . __( 'Tags:', 'bonestheme' ) . '</span> ', ', ', '</p>' ); ?>
 
 								</footer>
