6e13fb77879bebdb1a9813e231c66d853d2aab84
Alex Fuller <info@alexfuller.co.uk>
2014-04-16 22:02:20 +0100

    Flannel

---
+++ header.php
@@ -0,0 +1,51 @@
+<!doctype html>
+
+<!--[if lt IE 7]><html <?php language_attributes(); ?> class="no-js lt-ie9 lt-ie8 lt-ie7"><![endif]-->
+<!--[if (IE 7)&!(IEMobile)]><html <?php language_attributes(); ?> class="no-js lt-ie9 lt-ie8"><![endif]-->
+<!--[if (IE 8)&!(IEMobile)]><html <?php language_attributes(); ?> class="no-js lt-ie9"><![endif]-->
+<!--[if gt IE 8]><!--> <html <?php language_attributes(); ?> class="no-js"><!--<![endif]-->
+
+<head>
+	<meta charset="utf-8">
+
+	<!-- Google Chrome Frame for IE -->
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+	<title><?php wp_title( '|', true, 'right' ); ?></title>
+
+	<!-- mobile meta (hooray!) -->
+	<meta name="HandheldFriendly" content="True">
+	<meta name="MobileOptimized" content="320">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+	<!-- icons & favicons -->
+	<link rel="apple-touch-icon" href="<?php echo get_template_directory_uri(); ?>/library/images/apple-icon-touch.png">
+	<link rel="icon" href="<?php echo get_template_directory_uri(); ?>/favicon.png">
+	<!--[if IE]>
+		<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico">
+	<![endif]-->
+	<meta name="msapplication-TileColor" content="#f01d4f">
+	<meta name="msapplication-TileImage" content="<?php echo get_template_directory_uri(); ?>/library/images/win8-tile-icon.png">
+
+	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
+
+	<!-- wordpress head functions -->
+	<?php wp_head(); ?>
+	<!-- end of wordpress head -->
+
+	<!-- drop Google Analytics Here -->
+	<!-- end analytics -->
+
+</head>
+
+<body <?php body_class(); ?>>
+
+<!-- Side navigation must be outside the wrapper -->
+<?php get_sidebar(); ?>
+
+<!-- wrapper -->
+<div id="wrapper">
+
+	<?php include '_header.php'; ?>
+
+	<div class="content" role="main">
