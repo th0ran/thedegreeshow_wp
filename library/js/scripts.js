5c64a30de1e24896ed2e6dc74c30324ce9ef3c5c
Zoe Bryant <zoeabryant@gmail.com>
2014-05-15 14:31:38 +0100

    Map no longer hijacks the scrolling

--- library/js/scripts.js
+++ library/js/scripts.js
@@ -248,6 +248,8 @@ function init() {
     // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
     var mapOptions = {
         // How zoomed in you want the map to start at (always required)
+        
+	    scrollwheel: false,
         zoom: 16,
 
 		disableDefaultUI: true,
