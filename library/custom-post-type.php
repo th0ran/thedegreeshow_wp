8b79173d9c6cfd10f4446b3c67a50fdcf9dfc243
Alex Fuller <info@alexfuller.co.uk>
2014-05-22 22:29:14 +0100

    Team Update

--- library/custom-post-type.php
+++ library/custom-post-type.php
@@ -28,8 +28,8 @@ function the_team_custom_posts() {
 			'query_var' => true,
 			'menu_position' => 8, /* this is what order you want it to appear in on the left hand side menu */ 
 			'menu_icon' => get_stylesheet_directory_uri() . '/library/images/custom-post-icon.png', /* the icon for the custom post type menu */
-			'rewrite'	=> array( 'slug' => 'the_team', 'with_front' => false ), /* you can specify its url slug */
-			'has_archive' => 'team', /* you can rename the slug here */
+			'rewrite'	=> array( 'slug' => 'team', 'with_front' => false ), /* you can specify its url slug */
+			'has_archive' => 'the_team', /* you can rename the slug here */
 			'capability_type' => 'post',
 			'hierarchical' => false,
 			/* the next one is important, it tells what's enabled in the post editor */
